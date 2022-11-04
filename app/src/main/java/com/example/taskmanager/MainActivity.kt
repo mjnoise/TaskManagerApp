5d30d4c1d3f491c2b1c21d03b810da0318b5f781
mjnoise <minaj.sheikh4@outlook.com>
2022-11-04 20:36:15 +0530

    Done with the page

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.taskmanager
 
+import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import androidx.compose.material.Text as Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +43,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskManagerApp() {
+    TaskManagerPage(
+        image = painterResource(id = R.drawable.ic_task_completed),
+        taskStatus = stringResource(R.string.task_completed),
+        emotionText = stringResource(R.string.emotion_text)
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun TaskManagerPage(image: Painter, taskStatus : String, emotionText : String) {
+    Column (verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+        Image(
+            painter = image,
+            contentDescription = "Task Completion Tick Art",
+            modifier = Modifier.wrapContentSize(Alignment.Center)
+        )
+
+        Text(
+            text = taskStatus,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+
+        Text(
+            text = emotionText,
+            fontSize = 16.sp
+        )
+
+
+
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun TaskManagerPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        TaskManagerApp()
     }
 }
